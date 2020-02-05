dbf6b5463d7c0a0d9a8c2b646d00ce3d209c2ff3
pengyunfei <stack_admin@163.com>
2020-02-05 20:17:43 +0800

    feat:作用域/AOGO

---
+++ 作用域/1.js
@@ -0,0 +1,50 @@
+// function test(){}
+// console.log(test.[[scope]])
+
+function a() {
+  function b() {
+    var b = 234;
+  }
+  var a = 123;
+  b();
+}
+var global = 100;
+a();
+
+/** AO-->Activation Object  GO-->Global Object
+ * a defined a.[[scope]] -->  0 : GO {}
+ * a doing a.[[scope]] -->  0 : AO {}  1 : GO {}
+ */
+
+// test
+function a() {
+  function b() {
+    function c() {
+    }
+    c()
+  }
+  b()
+}
+a()
+
+/** AO-->Activation Object    GO-->Global Object
+ * a defined a.[[scope]] -->  0 : GO  
+ * a doing   a.[[scope]] -->  0 : aAO   
+ *                            1 : GO
+ *   
+ * b defined b.[[scope]] -->  0 : aAO  
+ *                            1 : GO  
+ * 
+ * b doing   b.[[scope]] -->  0 : bAO   
+ *                            1 : aAO  
+ *                            2 : GO
+ *   
+ * c defined c.[[scope]] -->  0 : bAO   
+ *                            1 : aAO  
+ *                            2 : GO
+ *  
+ * c doing   c.[[scope]] -->  0 : cAO   
+ *                            1 : bAO  
+ *                            2 : aAO  
+ *                            3 : GO 
+ */

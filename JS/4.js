89917d473edf3cddcd79c7f0af8cb8cf8b736790
pengyunfei <stack_admin@163.com>
2020-02-29 13:42:00 +0800

    feat:vue-dindow call/apply

--- JS/4.js
+++ JS/4.js
@@ -13,4 +13,31 @@ test.call()
   // console.log(a)
   Person.call(obj, '飞')
   console.log(obj)
-} 
+}
+
+
+{
+
+  Function.prototype.call = function (target, ...args) {
+    const context = target;
+    const fn = Symbol();
+    context[fn] = this;
+    const result = context[fn](...args);
+    delete context[fn];
+    return result;
+  }
+  Function.prototype.apply = function (target, args) {
+    const context = target;
+    const fn = Symbol();
+    context[fn] = this;
+    const result = context[fn](...args);
+    delete context[fn];
+    return result;
+  }
+
+  function test(test1, test2) {
+    console.log(this.a + test1 + test2)
+  }
+  const testObj = { a: 1 }
+  test.apply(testObj, [1, 2])
+}
